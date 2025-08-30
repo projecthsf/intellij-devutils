e9dbea1cead6274a8a4ac567f0c69861cf5d5588
linhnnmt <162856460+linhnnmt@users.noreply.github.com>
2025-08-30 07:50:29 +0700

    Update DevUtilsToolWindowFactory.java

--- src/main/java/io/github/projecthsf/devutils/toolWindow/DevUtilsToolWindowFactory.java
+++ src/main/java/io/github/projecthsf/devutils/toolWindow/DevUtilsToolWindowFactory.java
@@ -6,8 +6,7 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import io.github.projecthsf.devutils.toolWindow.contents.ApplyDatasetWindowPanel;
-import io.github.projecthsf.devutils.toolWindow.contents.SqlToDTOWindowPanel;
+import io.github.projecthsf.devutils.toolWindow.controller.ApplyDatasetWindowController;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -15,7 +14,7 @@ import javax.swing.*;
 public class DevUtilsToolWindowFactory implements ToolWindowFactory, DumbAware {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        toolWindow.getContentManager().addContent(createContent("Apply Dataset", new ApplyDatasetWindowPanel(toolWindow)));
+        toolWindow.getContentManager().addContent(createContent("Apply Dataset", new ApplyDatasetWindowController(toolWindow)));
         //toolWindow.getContentManager().addContent(createContent("Sql to DTO", new SqlToDTOWindowPanel(toolWindow)));
     }
 
