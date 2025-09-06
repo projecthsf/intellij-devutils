55bee1ee9e1554f049cd44f3ea8c9e5207a67373
linhnnmt <162856460+linhnnmt@users.noreply.github.com>
2025-09-06 18:57:20 +0700

    Delete src/main/java/io/github/projecthsf/devutils/forms/settings/CommonMasterDetailForm.java

--- src/main/java/io/github/projecthsf/devutils/forms/settings/CommonMasterDetailForm.java
+++
@@ -1,7 +0,0 @@
-package io.github.projecthsf.devutils.forms.settings;
-
-import javax.swing.*;
-
-public abstract class CommonMasterDetailForm extends JPanel {
-    public abstract void updateForm(String itemName);
-}
