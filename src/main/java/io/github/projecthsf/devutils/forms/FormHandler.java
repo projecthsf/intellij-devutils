b1a9bd9c57302c4908d61a4f3fd3c8808382b26c
linhnnmt <162856460+linhnnmt@users.noreply.github.com>
2025-09-07 10:12:22 +0700

    Update FormHandler.java

--- src/main/java/io/github/projecthsf/devutils/forms/FormHandler.java
+++ src/main/java/io/github/projecthsf/devutils/forms/FormHandler.java
@@ -1,4 +1,12 @@
 package io.github.projecthsf.devutils.forms;
 
 public class FormHandler {
+    protected boolean modified = false;
+    public boolean isModified() {
+        return modified;
+    }
+
+    public void setModified(boolean modified) {
+        this.modified = modified;
+    }
 }
