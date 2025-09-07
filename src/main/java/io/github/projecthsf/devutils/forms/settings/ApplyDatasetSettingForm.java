d3bf2478dc6620553eca919ee201ec9bf4447ec0
linhnnmt <162856460+linhnnmt@users.noreply.github.com>
2025-09-07 08:28:17 +0700

    Update ApplyDatasetSettingForm.java

--- src/main/java/io/github/projecthsf/devutils/forms/settings/ApplyDatasetSettingForm.java
+++ src/main/java/io/github/projecthsf/devutils/forms/settings/ApplyDatasetSettingForm.java
@@ -5,19 +5,5 @@ import io.github.projecthsf.devutils.forms.toolWindows.ApplyDatasetWindowForm;
 import io.github.projecthsf.devutils.utils.ActionUtil;
 
 public class ApplyDatasetSettingForm extends ApplyDatasetWindowForm {
-    public void updateForm(
-            String displayName,
-            CsvSeparatorEnum csvSeparator,
-            String dataset,
-            String codeTemplate
-    ) {
-        separartor.setSelectedItem(csvSeparator);
-        ActionUtil.updateText(dataSet, dataset);
-        ActionUtil.updateText(this.codeTemplate, codeTemplate);
-    }
-
-    public String getCodeTemplate() {
-        return codeTemplate.getDocument().getText();
-    }
 
 }
