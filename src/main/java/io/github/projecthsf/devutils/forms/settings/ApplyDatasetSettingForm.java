86f184eebb3a42bcccdb5c9d7d58d1f0a6069abf
linhnnmt <162856460+linhnnmt@users.noreply.github.com>
2025-09-07 11:21:04 +0700

    Update ApplyDatasetSettingForm.java

--- src/main/java/io/github/projecthsf/devutils/forms/settings/ApplyDatasetSettingForm.java
+++ src/main/java/io/github/projecthsf/devutils/forms/settings/ApplyDatasetSettingForm.java
@@ -1,9 +1,7 @@
 package io.github.projecthsf.devutils.forms.settings;
 
-import io.github.projecthsf.devutils.enums.CsvSeparatorEnum;
-import io.github.projecthsf.devutils.forms.toolWindows.ApplyDatasetWindowForm;
-import io.github.projecthsf.devutils.utils.ActionUtil;
+import io.github.projecthsf.devutils.forms.toolWindows.DatasetSnippetWindowForm;
 
-public class ApplyDatasetSettingForm extends ApplyDatasetWindowForm {
+public class DatasetSnippetSettingForm extends DatasetSnippetWindowForm {
 
 }
